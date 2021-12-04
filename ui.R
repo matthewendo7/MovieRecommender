a4e2a774d27baeb997a8078eb907dfc219410ccc
matthewendo7 <40447661+matthewendo7@users.noreply.github.com>
2021-12-04 00:32:16 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -10,32 +10,32 @@ source('functions/helpers.R')
 
 shinyUI(
     dashboardPage(
-          skin = "blue",
-          dashboardHeader(title = "Book Recommender"),
-          
-          dashboardSidebar(disable = TRUE),
-
-          dashboardBody(includeCSS("css/books.css"),
-              fluidRow(
-                  box(width = 12, title = "Step 1: Rate as many books as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
-                      div(class = "rateitems",
-                          uiOutput('ratings')
+        skin = "blue",
+        dashboardHeader(title = "Movie Recommender"),
+        
+        dashboardSidebar(disable = TRUE),
+        
+        dashboardBody(includeCSS("css/movies.css"),
+                      fluidRow(
+                          box(width = 12, title = "Step 1: Rate as many movies as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                              div(class = "rateitems",
+                                  uiOutput('ratings')
+                              )
+                          )
+                      ),
+                      fluidRow(
+                          useShinyjs(),
+                          box(
+                              width = 12, status = "info", solidHeader = TRUE,
+                              title = "Step 2: Discover movies you might like",
+                              br(),
+                              withBusyIndicatorUI(
+                                  actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+                              ),
+                              br(),
+                              tableOutput("results")
+                          )
                       )
-                  )
-                ),
-              fluidRow(
-                  useShinyjs(),
-                  box(
-                    width = 12, status = "info", solidHeader = TRUE,
-                    title = "Step 2: Discover books you might like",
-                    br(),
-                    withBusyIndicatorUI(
-                      actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
-                    ),
-                    br(),
-                    tableOutput("results")
-                  )
-               )
-          )
+        )
     )
 ) 
