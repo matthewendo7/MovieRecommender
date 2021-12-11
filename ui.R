38dc80268df5fe077b5c9254c8759af24e3f604a
Asiri <88098929+wdsilva2@users.noreply.github.com>
2021-12-11 14:15:35 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -9,88 +9,43 @@ library(shinyjs)
 source('functions/helpers.R')
 
 shinyUI(
-    dashboardPage(
-        skin = "blue",
-        dashboardHeader(title = "Movie Recommender"),
-        dashboardSidebar(
-            sidebarMenu(
-                menuItem("Movie Recommend", tabName = "movieRecommend"),
-                menuItem("Genre", tabName = "genre")
-            )
-        ),
-        dashboardBody(
-            tabItems(
-                tabItem("movieRecommend",
-                        includeCSS("css/movies.css"),
-                          fluidRow(
-                              box(width = 12, title = "Step 1: Rate as many movies as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
-                                  div(class = "rateitems",
-                                      uiOutput('ratings')
-                                  )
-                              )
-                          ),
-                          fluidRow(
-                              useShinyjs(),
-                              box(
-                                  width = 12, status = "info", solidHeader = TRUE,
-                                  title = "Step 2: Discover movies you might like",
-                                  br(),
-                                  withBusyIndicatorUI(
-                                      actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
-                                  ),
-                                  br(),
-                                  tableOutput("results")
-                              )
-                          )
+  dashboardPage(
+    skin = "blue",
+    dashboardHeader(title = "Movie Recommender"),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Movie Recommend", tabName = "movieRecommend"),
+        menuItem("Genre", tabName = "genre")
+      )
+    ),
+    dashboardBody(
+      tabItems(
+        tabItem("movieRecommend",
+                includeCSS("css/movies.css"),
+                fluidRow(
+                  box(width = 12, title = "Step 1: Rate as many movies as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                      div(class = "rateitems",
+                          uiOutput('ratings')
+                      )
+                  )
                 ),
-              tabItem("genre"
-                     
-                     
-                     
-                     
-                     
-                     
-                     
-                     )
-            )
-        )
+                fluidRow(
+                  useShinyjs(),
+                  box(
+                    width = 12, status = "info", solidHeader = TRUE,
+                    title = "Step 2: Discover movies you might like",
+                    br(),
+                    withBusyIndicatorUI(
+                      actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+                    ),
+                    br(),
+                    tableOutput("results")
+                  )
+                )
+        ),
+        tabItem("genre")
+      )
     )
+  )
 ) 
 
-
-
-
-
-
-
-# shinyUI(
-#     dashboardPage(
-#         skin = "blue",
-#         dashboardHeader(title = "Movie Recommender"),
-#         
-#         dashboardSidebar(),
-#         
-#         dashboardBody(includeCSS("css/movies.css"),
-#                       fluidRow(
-#                           box(width = 12, title = "Step 1: Rate as many movies as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
-#                               div(class = "rateitems",
-#                                   uiOutput('ratings')
-#                               )
-#                           )
-#                       ),
-#                       fluidRow(
-#                           useShinyjs(),
-#                           box(
-#                               width = 12, status = "info", solidHeader = TRUE,
-#                               title = "Step 2: Discover movies you might like",
-#                               br(),
-#                               withBusyIndicatorUI(
-#                                   actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
-#                               ),
-#                               br(),
-#                               tableOutput("results")
-#                           )
-#                       )
-#         )
-#     )
-# ) 
