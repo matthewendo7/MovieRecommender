363371f5c119a7928b8b5a159a7ca1ade6a409e0
Asiri <88098929+wdsilva2@users.noreply.github.com>
2021-12-11 14:19:01 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -44,11 +44,12 @@ shinyUI(
                 )
         ),
         tabItem("genre",
+                fluidRow(
                 column(2,selectInput("genre_input", "Select a Movie Genre:",
                                                   c("Action","Adventure","Animation","Children's","Comedy","Crime","Documentary","Drama","Fantasy","Film-Noir","Horror","Musical",
                                                      "Mystery","Romance","Sci-Fi","Thriller","War","Western"))),
                 column(2,selectInput("num_movies", "Number of Movies to Show:", c(3,5,10)))
-              
+              )
                )
       )
     )
