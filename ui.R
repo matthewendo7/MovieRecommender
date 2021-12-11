c83be938d2c60b507cd7dacce599e773db0a38c6
Asiri <88098929+wdsilva2@users.noreply.github.com>
2021-12-11 14:54:25 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -48,7 +48,7 @@ shinyUI(
                             selectInput("genre_input", "Select a Movie Genre:",
                                                   list("Action","Adventure","Animation","Children's","Comedy","Crime","Documentary","Drama","Fantasy","Film-Noir","Horror","Musical",
                                                      "Mystery","Romance","Sci-Fi","Thriller","War","Western")),
-                            selectInput("num_movies_input", "1Number of Movies to Show:", list(3,5,10))
+                            selectInput("num_movies_input", "1Number of Movies to Show:", list("3","5","10"))
                            
                
                
