623a69d2dba8631079f63c27ab771a16ada458b1
Asiri <88098929+wdsilva2@users.noreply.github.com>
2021-12-12 19:07:43 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -51,7 +51,7 @@ shinyUI(
                             column(2,selectInput("num_movies", "Number of Movies to Show:", list("Top 3","Top 5","Top 10"))),
                             column(2,style='padding: 20px 0;',
                             #withBusyIndicatorUI(
-                            actionButton("btn_show_genre_results", "Show Movie Results")),
+                            actionButton("btn_show_genre_results", "Show Movie Results", class = "btn-warning")),
                             #    ),
                             column(6,)
                             
